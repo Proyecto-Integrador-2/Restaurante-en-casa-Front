fb942c3198d5cf25dd0e237978fc01a0b95b679e
veluaru <angie.castaneda@udea.edu.co>
2020-11-04 20:04:41 -0500

    consumir servicios de login y register

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,17 +3,27 @@
     <v-card elevation="6" class="mx-auto pa-5" max-width="900" min-height="500">
       <v-card-title class="justify-center">Login</v-card-title>
       <v-container class="d-flex justify-center flex-column">
-        <v-text-field label="Email" outlined></v-text-field>
+        <v-text-field v-model="email" label="Email" outlined></v-text-field>
         <v-text-field
+          v-model="password"
           :type="'Password'"
           label="Password"
           outlined
         ></v-text-field>
-        <v-btn style="max-width: 300px" color="orange accent-2" dark large
+      </v-container>
+      <v-container class="d-flex flex-column" align-center>
+        <v-btn
+          class="justify-center"
+          style="max-width: 300px"
+          color="orange accent-2"
+          @click="submit"
+          dark
+          large
           >Submit</v-btn
         >
         <p></p>
         <v-btn
+          class="justify-center"
           style="max-width: 300px"
           @click="register"
           color="orange accent-2"
@@ -27,18 +37,38 @@
 </template>
 
 <script>
+
+import loginServices from "../services/login.services";
+
 export default {
   name: "Login",
   data() {
-    return {};
+    return {
+      email:"",
+      password:"",
+      token:""
+    };
   },
   methods: {
-    home() {
-      this.$router.push({ name: "Home" });
-    },
     register() {
       this.$router.push({ name: "Register" });
     },
+    async submit() {
+      if(this.email!="" && this.password!=""){
+        try {
+          let params = {mail: this.email, password: this.password};
+          let response = await loginServices.postLogin(params);
+          console.log(response);
+          localStorage.setItem('token',response.data);
+          this.$router.push({ name: "Restaurants" });
+        } catch (error) {
+          console.log("Fallo el Login");
+          console.log(error);
+        }
+      } else {
+        console.log("No hay nada escrito");
+      }
+    }
   },
 };
 </script>
@@ -50,7 +80,7 @@ export default {
   align-items: center;
   height: 100%;
 }
-.v-container { 
+.v-container {
   justify-items: center !important;
 }
 </style>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,38 +1,99 @@
 <template>
   <div class="Login pa-5">
-    <v-card
-      elevation="6"
-      class="mx-auto pa-5"
-      max-width="900"
-      min-height="500"
-    >
+    <v-card elevation="6" class="mx-auto pa-5" max-width="900" min-height="500">
       <v-card-title class="justify-center">Registro</v-card-title>
       <v-container class="d-flex justify-center flex-column">
-        <v-text-field label="Nombre Completo" outlined></v-text-field>
-        <v-text-field label="Correo Electrónico" outlined></v-text-field>
-        <v-text-field label="Número de celular" outlined></v-text-field>
+        <v-text-field
+          v-model="name"
+          label="Nombre Completo"
+          outlined
+        ></v-text-field>
+        <v-text-field
+          v-model="mail"
+          label="Correo Electrónico"
+          outlined
+        ></v-text-field>
+        <v-text-field
+          v-model="number"
+          label="Número de celular"
+          type="number"
+          outlined
+        ></v-text-field>
         <v-combobox
-          v-model="select"
+          v-model="documentType"
           :items="items"
           label="Tipo de Documento"
+          type="number"
           outlined
           allowCustom="False"
         ></v-combobox>
-        <v-text-field label="Número de Documento" outlined></v-text-field>
-        <v-text-field label="Contraseña" outlined></v-text-field>
-        <v-text-field label="Repita la Contraseña" outlined></v-text-field>
+        <v-text-field
+          v-model="nrDocument"
+          label="Número de Documento"
+          type="number"
+          outlined
+        ></v-text-field>
+        <v-text-field v-model="password" label="Contraseña" outlined></v-text-field>
+        <v-text-field
+          label="Repita la Contraseña"
+          outlined
+        ></v-text-field>
+      </v-container>
+      <v-container class="d-flex flex-column ma-3" align-center>
+        <v-btn
+          class="justify-center"
+          style="max-width: 300px"
+          @click="register"
+          color="orange accent-2"
+          dark
+          large
+          align-center
+          >Submit</v-btn
+        >
       </v-container>
     </v-card>
   </div>
 </template>
 
 <script>
+import loginServices from "../services/login.services";
+
 export default {
   data() {
     return {
-      select: "",
-      items: [ "Tarjeta de Identidad", "Cédula de Ciudadania", "Cédula de Extrangeria", "Pasaporte"],
+      name: "",
+      mail: "",
+      number: "",
+      documentType: "0",
+      nrDocument: "",
+      password: "",
+      items: [
+        "Tarjeta de Identidad",
+        "Cédula de Ciudadania",
+        "Cédula de Extrangeria",
+        "Pasaporte",
+      ],
     };
   },
+  methods: {
+    async register () {
+      try {
+          let params = {
+          name: this.name,
+           mail: this.mail, 
+           number: this.number, 
+           documentType: this.documentType, 
+           nrDocument: this.nrDocument, 
+           password: this.password};
+          let response = await loginServices.postUser(params);
+          console.log(response);
+          alert("Se ha creado el usuario correctamente");
+          this.$router.push({ name: "Login" });
+        } catch (error) {
+          console.log("Fallo el registro");
+          console.log(error);
+        }
+    }
+  }
 };
 </script>
